985a5e80fb3ec9ff0b9664f16bab5729144c8274
Kyungsoo Hong <ronen@MacBook-Pro.local>
2020-07-12 17:25:07 +0900

    add initial naver_crawler code

---
+++ naver_crawler/naver_crawler/spiders/naver_crawler.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class NaverSpider(scrapy.Spider):
+    name = "naver"
+
+    def start_requests(self):
+        urls = [
+            "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=001&aid=0011729190"
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        content = response.xpath(
+            "//div[@id='articleBodyContents']//text()").getall()
+        self.log(content)
